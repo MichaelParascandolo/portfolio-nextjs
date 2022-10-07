f5007c7dd6f1cca443f7f99130fadcfadce87965
Michael Parascandolo <michaelparascandolo@me.com>
2022-10-07 10:02:25 -0400

    trying to fix logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -16,7 +16,7 @@ const Logo = () => {
         <Link href="/">
           <p className="font-logoMP text-[#ee2d2d] text-4xl cursor-pointer select-none inline-flex">
             {/* &#123;Mp&#125; */}
-            &#123; Mp &#125;
+            Mp
           </p>
         </Link>
       </motion.div>
