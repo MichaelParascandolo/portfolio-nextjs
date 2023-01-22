e29033e47462e5e4728cefbf7892ea35d822393c
Michael Parascandolo <michael.parascandolo@me.com>
2023-01-21 19:41:47 -0500

    added onTap animation to social icons

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -13,48 +13,54 @@ const Socials = ({ size, circles }: { size: number; circles: boolean }) => {
       duration: 0.2,
     },
   };
+  const circleTap = {
+    scale: [1.15, 0.9, 1.15],
+    transition: {
+      duration: 0.4,
+    },
+  };
   return (
     <>
-      <motion.div whileHover={circleHover}>
+      <motion.div whileHover={circleHover} whileTap={circleTap}>
         <a
           href="https://www.linkedin.com/in/michael-parascandolo-774725226/"
           target="_blank"
           rel="noreferrer"
         >
-          <div className={circles ? styles.circle : ""}>
+          <div className={circles ? styles.circle : null}>
             <FaLinkedinIn className={styles.color} size={size} />
           </div>
         </a>
       </motion.div>
-      <motion.div whileHover={circleHover}>
+      <motion.div whileHover={circleHover} whileTap={circleTap}>
         <a
           href="https://github.com/michaelparascandolo"
           target="_blank"
           rel="noreferrer"
         >
-          <div className={circles ? styles.circle : ""}>
+          <div className={circles ? styles.circle : null}>
             <FaGithub className={styles.color} size={size} />
           </div>
         </a>
       </motion.div>
-      <motion.div whileHover={circleHover}>
+      <motion.div whileHover={circleHover} whileTap={circleTap}>
         <a
           href="https://twitter.com/mikejpara"
           target="_blank"
           rel="noreferrer"
         >
-          <div className={circles ? styles.circle : ""}>
+          <div className={circles ? styles.circle : null}>
             <FaTwitter className={styles.color} size={size} />
           </div>
         </a>
       </motion.div>
-      <motion.div whileHover={circleHover}>
+      <motion.div whileHover={circleHover} whileTap={circleTap}>
         <a
           href="https://www.instagram.com/mikejpara/"
           target="_blank"
           rel="noreferrer"
         >
-          <div className={circles ? styles.circle : ""}>
+          <div className={circles ? styles.circle : null}>
             <FaInstagram className={styles.color} size={size} />
           </div>
         </a>
