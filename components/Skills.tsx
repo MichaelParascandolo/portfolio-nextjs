d3664ea78cd34d4ab0caf120c1d4da1bbeaaa826
Michael Parascandolo <michaelparascandolo@me.com>
2023-02-05 17:39:22 -0500

    added / changed skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -32,31 +32,21 @@ const Skills = () => {
     "Bootstrap",
     "React JS",
     "Next JS",
-  ];
-  const familiar: string[] = [
-    "SASS",
-    "Github",
-    "Python",
+    "Git",
     "Java",
-    "Firebase",
-    "React Native",
   ];
-  // other skills: electron, jQuery
+  const familiar: string[] = ["SASS", "Python", "Firebase", "React Native"];
+  // other skills: electron, jQuery, GitHub
   const styles = {
-    grid: "grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8", // gap 8 or 4
-    grid2: "grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8", // gap 8 or 4...
+    grid: "grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-8", // gap 8 or 4
+    grid2: "grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-8", // gap 8 or 4...
   };
   return (
     <div id="skills" className="w-full lg:h-screen p-2 dark:bg-[#1E1E1E]">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
         <p className="text-xl tracking-widest uppercase text-primary">Skills</p>
         <h2 className="py-4 dark:text-white">Experienced With</h2>
-        <motion.div
-          // ref={ref}
-          variants={variants}
-          initial="hidden"
-          animate={controls}
-        >
+        <motion.div variants={variants} initial="hidden" animate={controls}>
           <div className={styles.grid} ref={ref}>
             {experienced.map((item, index) => (
               <SkillItem name={item} key={index} />
@@ -64,12 +54,7 @@ const Skills = () => {
           </div>
         </motion.div>
         <h2 className="py-4 mt-5 dark:text-white">Some Experience With</h2>
-        <motion.div
-          // ref={ref}
-          variants={variants}
-          initial="hidden"
-          animate={controls}
-        >
+        <motion.div variants={variants} initial="hidden" animate={controls}>
           <div className={styles.grid2}>
             {familiar.map((item, index) => (
               <SkillItem name={item} key={index} />
