d1a23d8da33ace6ea2803290cd20ce5eb072afed
Michael Parascandolo <michaelparascandolo@me.com>
2023-05-07 23:40:45 -0400

    added interface for ProjectItemData

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -2,19 +2,15 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 
-const ProjectItem = ({
-  title,
-  backgroundImg,
-  description,
-  tech,
-  projectUrl,
-}: {
+interface ProjectItemData {
   title?: string;
   backgroundImg: any;
   description?: string;
   tech?: string;
   projectUrl?: string;
-}) => {
+}
+
+const ProjectItem = (props: ProjectItemData) => {
   const styles = {
     containerHover:
       "relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-0 group border-solid border-2 border-gray-200 hover:bg-gradient-to-r from-[#f26969] to-primary dark:bg-[#282828] dark:shadow-black dark:border-black",
@@ -36,32 +32,29 @@ const ProjectItem = ({
         },
       }}
     >
-      <div className={projectUrl ? styles.containerHover : styles.container}>
+      <div
+        className={props.projectUrl ? styles.containerHover : styles.container}
+      >
         <Image
           className="rounded-xl group-hover:opacity-10"
-          src={backgroundImg}
+          src={props.backgroundImg}
           draggable={false}
-          alt={title}
+          alt={props.title}
         />
         <div className="hidden group-hover:block w-[80%] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-          {/* <div className="text-center">
-            <span className="font-logoMP text-black text-4xl select-none">
-              MP
-            </span>
-          </div> */}
           <h3 className="text-4xl text-gray-900 tracking-wide text-center">
-            {title}
+            {props.title}
           </h3>
           <p className="pb-0 pt-1 text-gray-800 text-center font-bold text-lg tracking-wide">
-            {description}
+            {props.description}
           </p>
           <p className="py-1 text-gray-800 text-center tracking-wide font-semibold">
-            {tech}
+            {props.tech}
           </p>
           {/* make this a button */}
-          {projectUrl ? (
+          {props.projectUrl ? (
             <div className="flex justify-center">
-              <Link href={projectUrl}>
+              <Link href={props.projectUrl}>
                 <button className="text-center py-1 text-lg cursor-pointer w-[200px] tracking-widest shadow-md shadow-gray-900 border-gray-900 border-2 hover:bg-white/40 duration-300 ease-in-out transition-all">
                   More Info
                 </button>
